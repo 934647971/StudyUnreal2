e221ca5e1a2dbc2f3f6865fb76e548859ef82f44
WenGuang Zhou <wenguang.zhou@picovr.com>
2019-12-10 13:29:28 +0800

    task#0004:12月10日中午学习，属性部分一期。

---
+++ Source/StudyUnreal2/Public/GamePlay/ShinbiController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShinbiController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDYUNREAL2_API AShinbiController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/StudyUnreal2/StudyUnreal2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StudyUnreal2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StudyUnreal2, "StudyUnreal2" );
